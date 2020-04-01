56b63b388a836d0d785a7285205c3f1b6268d797
atleastzero <megan.obryan52@gmail.com>
2020-03-31 18:31:40 -0700

    Completed is_sorted, bubble_sort, and selection_sort functions.

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -5,27 +5,69 @@ def is_sorted(items):
     """Return a boolean indicating whether given items are in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Check that all adjacent items are in order, return early if so
+    # check each consecutive pair
+    #   if later is < previous
+    #       return false
+    # return true
 
+    # check if list length is less than 2 and therefore is in order
+    if len(items) < 2:
+        return True
+    
+    # i and i+1 represent each consecutive pair
+    for i in range(len(items)-1):
+        if items[i+1] < items[i]:
+            return False    # return early in case of counterexample to sorted
+    return True             # return true only when every pair checked
 
 def bubble_sort(items):
     """Sort given items by swapping adjacent items that are out of order, and
     repeating until all items are in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Swap adjacent items that are out of order
+    # return early if already sorted
+    if is_sorted(items):
+        return
 
+    # start with a true value
+    # - note: swapped will represent if a value has been swapped.
+    # -       if no value has been swapped, the list is already sorted.
+    # -       if the list has already been sorted, the function can end.
+    swapped = True
+    while swapped:
+        for i in range(len(items)-1):
+            swapped = False # can only be updated when true, keeps it accurate
+            if items[i] > items[i+1]: # check each pair
+                items[i], items[i+1] = items[i+1], items[i] # first swap
+                index = i # represents the "highlighted" index 
+                          # (where a smaller value has been found)
+                swapped = True # since we're found a swap, change our tracker
+                for j in range(i): # check each value before index
+                    if items[i-j-1] > items[index]: # found another swap, cont.
+                        items[index], items[i-j-1] = items[i-j-1], items[index]
+                        index = i-j-1
+                    else: # no more swapping for index! we can stop j's loop
+                        break
 
 def selection_sort(items):
     """Sort given items by finding minimum item, swapping it with first
     unsorted item, and repeating until all items are in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Find minimum item in unsorted items
-    # TODO: Swap it with first unsorted item
+    # if items length is 0 or 1, stop
+    if len(items) < 2:
+        return
 
+    # i is a marker for the next swapping point with the selection
+    for i in range(len(items)):
+        # minimum is the index where the lowest number from i on is
+        minimum = i
+        for j, item in enumerate(items[minimum:]):
+            # set minimum according to the next lowest number
+            if item < items[minimum]:
+                minimum = j+i 
+        # swap marker value and minimum value
+        items[i], items[minimum] = items[minimum], items[i]
 
 def insertion_sort(items):
     """Sort given items by taking first unsorted item, inserting it in sorted
@@ -35,3 +77,4 @@ def insertion_sort(items):
     # TODO: Repeat until all items are in sorted order
     # TODO: Take first unsorted item
     # TODO: Insert it in sorted order in front of items
+    #Pseudocode
